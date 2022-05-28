34d80e611b9ca01475c32b4adc65bce0f6e67ea5
AdrianStraszecki <adrianStraszecki@users.noreply.github.com>
2022-05-28 13:31:35 +0100

    File views.py added

---
+++ hello_world/views.py
@@ -0,0 +1,23 @@
+from hello_world import app
+from hello-world.formatter import get_formatted
+from hello_world.formatter import SUPPORTED, PLAIN
+
+from hello_world import app
+from hello_world.formater import get_formatted
+from hello_world.formater import SUPPORTED, PLAIN
+from flask import request
+
+moje_imie = "Adrian"
+msg = "Hello World!"
+
+@app.route('/')
+def index():
+    output = request.args.get('output')
+    if not output:
+        output = PLAIN
+    return get_formatted(msg, moje_imie,
+                         output.lower())
+
+@app.route('/outputs')
+def supported_output():
+    return ", ".join(SUPPORTED)
